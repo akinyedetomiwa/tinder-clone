e48a2d70278b9b76bf65e31be3b8c6d8d6743337
akinyedetomiwa <akinyede8080@gmail.com>
2023-05-23 20:19:51 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    // BEM class naming convention
+    <div className="app">
+      <h1> Lets build the MERN TINDER clone guys!!!</h1>
+
+      {/* Header*/}
+      {/* TinderCards*/}
+      {/* SwipeButtons */}
+    </div>
+  );
+}
+
+export default App;
